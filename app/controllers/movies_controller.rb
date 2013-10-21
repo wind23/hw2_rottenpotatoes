865f1d7ebf9adc5b91d691d0174ba51425dc7088
saasbook <saasbook@saasbook.(none)>
2013-10-21 00:07:18 -0700

    edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,27 +1,45 @@
 class MoviesController < ApplicationController
 
+  
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+    
   end
 
   def index
-    #@movies = Movie.all
+    @all_ratings = ['G','PG','PG-13','R']
     type = params[:sort_by]
-    if type == "title"
-        @movies = Movie.find(:all, :order => :title)
-        @sort_by = 1
-    else
-        if type == "release_date"
-            @movies = Movie.find(:all, :order => :release_date)
-            @sort_by = 2
-        else
-            @movies = Movie.find(:all)
-            @sort_by = 3
+    if params[:commit] == "Refresh"
+        @movies = []
+        @sort_by = 3
+       	Movie.all.each do |e|
+            if params[:ratings][e.rating]=='1'
+               @movies.push(e)
+            end
         end
+        
+    else
+       # krue=(:order =>:release_date)
+    #end
+	    if type == "title"
+		@movies = Movie.find(:all, :order => :title)
+		@sort_by = 1
+	    else
+		if type == "release_date"
+		    @movies = Movie.find(:all, :order => :release_date)
+		    @sort_by = 2
+		else
+		    @movies = Movie.find(:all)
+		    @sort_by = 3
+		end
+	    end
     end
   end
+
+  def 
 
   def new
     # default: render 'new' template
